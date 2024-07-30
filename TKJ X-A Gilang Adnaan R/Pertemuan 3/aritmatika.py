1db03f7788f8cdd3762fae0effdce9682feedb1a
Gilangadnaan <adnanrisman130@gmail.com>
2024-07-30 09:39:21 +0700

    Add files via upload

---
+++ TKJ X-A Gilang Adnaan R/Pertemuan 3/aritmatika.py
@@ -0,0 +1,9 @@
+bil1 = 8
+bil2 = 2
+
+print ("hasil :", bil1 + bil2)
+
+print ("hasil :", bil1 - bil2)
+print ("hasil :", bil1 * bil2)
+print ("hasil :", bil1 / bil2)
+print ("hasil :", bil1 // bil2)
---
+++ TKJ X-A Gilang Adnaan R/pertemuan 1/perkenalan.py
@@ -0,0 +1,4 @@
+nama = "Gilang Adnaan R"
+kelas = "TKJ X A"
+hobi = "bermain bola"
+print(nama, kelas, hobi)    
